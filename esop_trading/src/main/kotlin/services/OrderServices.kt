a8e560ac640b216acc4980abb8616ad4df4216df
AkankshaChaudhari24 <akankshac@sahaj.ai>
2023-02-08 21:11:04 +0530

    add order service

--- esop_trading/src/main/kotlin/services/OrderServices.kt
+++ esop_trading/src/main/kotlin/services/OrderServices.kt
@@ -3,62 +3,41 @@ package services
 import models.DataStorage
 import models.Order
 import models.OrderExecutionLogs
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import repositories.OrderRepository
+import repositories.UserRepository
 import java.math.BigInteger
 import kotlin.math.min
 import kotlin.math.roundToLong
 
 
-class Util {
+class OrderServices {
     companion object {
-
-        @Synchronized
-        fun generateOrderId(): Long {
-            return DataStorage.orderId++
-        }
-
-        @Synchronized
-        fun generateOrderExecutionId(): Long {
-            return DataStorage.orderExecutionId++
-        }
-
-        @Synchronized
-        fun addOrderToBuyList(order: Order) {
-            DataStorage.buyList.add(order)
-        }
-
-        @Synchronized
-        fun addOrderToSellList(order: Order) {
-            DataStorage.sellList.add(order)
-        }
-
-        @Synchronized
-        fun addOrderToPerformanceSellList(order: Order) {
-            DataStorage.performanceSellList.add(order)
-        }
+        val logger: Logger = LoggerFactory.getLogger(OrderServices::class.java)
 
         fun matchOrders() {
-            val buyOrders = DataStorage.buyList
-            if(buyOrders.isEmpty()) return
+            val buyOrders = OrderRepository.getBuyList()
+            if (buyOrders.isEmpty()) return
             val currentBuyOrder = buyOrders.poll()
             matchWithPerformanceSellOrders(currentBuyOrder)
             matchWithNonPerformanceSellOrders(currentBuyOrder)
 
-            if(currentBuyOrder.remainingOrderQuantity > 0) buyOrders.add(currentBuyOrder)
+            if (currentBuyOrder.remainingOrderQuantity > 0) buyOrders.add(currentBuyOrder)
             else matchOrders()
         }
 
         private fun matchWithPerformanceSellOrders(buyOrder: Order) {
-            val performanceSellOrders = DataStorage.performanceSellList.iterator()
+            val performanceSellOrders = OrderRepository.getPerformanceSellList().iterator()
             while (performanceSellOrders.hasNext() && buyOrder.remainingOrderQuantity > 0) {
                 val currentPerformanceSellOrder = performanceSellOrders.next()
                 processOrder(buyOrder, currentPerformanceSellOrder, true)
-                if (currentPerformanceSellOrder.remainingOrderQuantity == 0L)
-                    performanceSellOrders.remove()
+                if (currentPerformanceSellOrder.remainingOrderQuantity == 0L) performanceSellOrders.remove()
             }
         }
 
         private fun matchWithNonPerformanceSellOrders(buyOrder: Order) {
-            val sellOrders = DataStorage.sellList
+            val sellOrders = OrderRepository.getSellList()
             while (sellOrders.isNotEmpty() && buyOrder.remainingOrderQuantity > 0) {
                 val currentSellOrder = sellOrders.poll()
 
@@ -66,8 +45,7 @@ class Util {
                 //If the top of the heap is not good enough, no point searching further
                 if (currentSellOrder.price > buyOrder.price) break
                 processOrder(buyOrder, currentSellOrder, false)
-                if (currentSellOrder.remainingOrderQuantity > 0)
-                    sellOrders.add(currentSellOrder)
+                if (currentSellOrder.remainingOrderQuantity > 0) sellOrders.add(currentSellOrder)
             }
         }
 
@@ -80,10 +58,13 @@ class Util {
                 updateSellerInventoryAndWallet(sellOrder, orderQuantity, orderExecutionPrice, isPerformanceESOP)
                 updateBuyerInventoryAndWallet(buyOrder, orderQuantity, orderExecutionPrice)
 
-                val orderFee = (orderAmount*DataStorage.COMMISSION_FEE_PERCENTAGE*0.01).roundToLong()
+
+                val orderFee = (orderAmount * DataStorage.COMMISSION_FEE_PERCENTAGE * 0.01).roundToLong()
+                logger.info("Order fee - {}", orderFee)
+
                 DataStorage.TOTAL_FEE_COLLECTED = DataStorage.TOTAL_FEE_COLLECTED + BigInteger.valueOf(orderFee)
                 val orderExecutionLog =
-                    OrderExecutionLogs(generateOrderExecutionId(), orderExecutionPrice, orderQuantity)
+                    OrderExecutionLogs(OrderRepository.generateOrderExecutionId(), orderExecutionPrice, orderQuantity)
                 sellOrder.addOrderExecutionLogs(orderExecutionLog)
                 buyOrder.addOrderExecutionLogs(orderExecutionLog)
             }
@@ -94,27 +75,23 @@ class Util {
         }
 
         private fun updateSellerInventoryAndWallet(
-            sellOrder: Order,
-            orderQuantity: Long,
-            orderExecutionPrice: Long,
-            isPerformanceESOP: Boolean
+            sellOrder: Order, orderQuantity: Long, orderExecutionPrice: Long, isPerformanceESOP: Boolean
         ) {
-            val seller = DataStorage.userList[sellOrder.userName]!!
+            val seller = UserRepository.getUser(sellOrder.userName)
             val orderAmount = orderQuantity * orderExecutionPrice
-            seller.updateLockedInventory(orderQuantity, isPerformanceESOP)
+            seller!!.updateLockedInventory(orderQuantity, isPerformanceESOP)
             seller.addMoneyToWallet((orderAmount * (1 - DataStorage.COMMISSION_FEE_PERCENTAGE * 0.01)).roundToLong())
         }
 
         private fun updateBuyerInventoryAndWallet(buyOrder: Order, orderQuantity: Long, orderExecutionPrice: Long) {
-            val buyer = DataStorage.userList[buyOrder.userName]!!
+            val buyer = UserRepository.getUser(buyOrder.userName)!!
             val orderAmount = orderQuantity * orderExecutionPrice
             buyer.updateLockedMoney(orderAmount)
             buyer.addEsopToInventory(orderQuantity)
 
             //Need to send difference back to free wallet when high buy and low sell are paired
             if (buyOrder.price > orderExecutionPrice) {
-                val amountToBeMovedFromLockedWalletToFreeWallet =
-                    orderQuantity * (buyOrder.price - orderExecutionPrice)
+                val amountToBeMovedFromLockedWalletToFreeWallet = orderQuantity * (buyOrder.price - orderExecutionPrice)
                 buyer.updateLockedMoney(amountToBeMovedFromLockedWalletToFreeWallet)
                 buyer.addMoneyToWallet(amountToBeMovedFromLockedWalletToFreeWallet)
             }
