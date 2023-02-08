8a45c4b91619e6873df4d5cdcb375bfe5ccdcde1
AkankshaChaudhari24 <akankshac@sahaj.ai>
2023-02-08 21:13:44 +0530

    refactor: using user repository to save user

--- esop_trading/src/main/kotlin/services/User.kt
+++ esop_trading/src/main/kotlin/services/User.kt
@@ -1,10 +1,7 @@
 package services
-
-import models.DataStorage
 import models.User
+import repositories.UserRepository
 
 fun saveUser(user: User) {
-    DataStorage.userList[user.username] = user
-    DataStorage.registeredEmails.add(user.emailId)
-    DataStorage.registeredPhoneNumbers.add(user.phoneNumber)
+    UserRepository.addUserInfo(user)
 }
