2fd4410f1783c8ac4d214e793009b7101ebe60b4
AkankshaChaudhari24 <akankshac@sahaj.ai>
2023-02-08 13:19:35 +0530

    refactor: return lifted out of if

--- esop_trading/src/main/kotlin/services/BuyOrderingComparator.kt
+++ esop_trading/src/main/kotlin/services/BuyOrderingComparator.kt
@@ -4,17 +4,17 @@ import models.Order
 class BuyOrderingComparator {
     companion object : Comparator<Order> {
         override fun compare(o1: Order, o2: Order): Int {
-            if (o1.orderPrice != o2.orderPrice) {
-                if (o1.orderPrice < o2.orderPrice) {
-                    return 1
+            return if (o1.price != o2.price) {
+                if (o1.price < o2.price) {
+                    1
                 } else {
-                    return -1
+                    -1
                 }
             } else {
                 if (o1.orderId > o2.orderId) {
-                    return 1
+                    1
                 } else {
-                    return -1
+                    -1
                 }
             }
         }
