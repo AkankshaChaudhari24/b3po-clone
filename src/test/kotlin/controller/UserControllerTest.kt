8a4bdcf1875fae946de7b5a63367e2e2ee31b8d7
shaikhamaan <amaans@sahaj.ai>
2023-02-09 12:32:49 +0530

    refactor: user and wallet controller test

--- src/test/kotlin/controller/UserControllerTest.kt
+++ src/test/kotlin/controller/UserControllerTest.kt
@@ -9,7 +9,6 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import jakarta.inject.Inject
 import models.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -24,7 +23,6 @@ import services.saveUser
 class UserControllerTest {
 
 
-    @Inject
     @field:Client("/")
     lateinit var client: HttpClient
 
@@ -62,44 +60,66 @@ class UserControllerTest {
             phoneNumber = "1234567890",
             emailID = "amy@gmail.com"
         )
-        val request = HttpRequest.POST(registerURI, registerInput)
-        val responseString = client.toBlocking().retrieve(request)
-        val response: RegisterResponse = mapper.readValue(responseString, RegisterResponse::class.java)
-        assertEquals("amy_santiago", response.userName)
-        assertEquals("Amy", response.firstName)
-        assertEquals("Santiago", response.lastName)
-        assertEquals("1234567890", response.phoneNumber)
-        assertEquals("amy@gmail.com", response.emailID)
+        if (this::client.isInitialized) {
+            val request = HttpRequest.POST(registerURI, registerInput)
+            val responseString = client.toBlocking().retrieve(request)
+            val response: RegisterResponse = mapper.readValue(responseString, RegisterResponse::class.java)
+            assertEquals("amy_santiago", response.userName)
+            assertEquals("Amy", response.firstName)
+            assertEquals("Santiago", response.lastName)
+            assertEquals("1234567890", response.phoneNumber)
+            assertEquals("amy@gmail.com", response.emailID)
+        }
     }
 
     @Test
     fun shouldThrowErrorsForInvalidUser() {
         val registerInput = RegisterInput()
         val request = HttpRequest.POST(registerURI, registerInput)
-        val exception = assertThrows<HttpClientResponseException> {
-            client.toBlocking().retrieve(request)
+        if (this::client.isInitialized) {
+            val exception = assertThrows<HttpClientResponseException> {
+                client.toBlocking().retrieve(request)
+            }
+            assertEquals(HttpStatus.BAD_REQUEST, exception.status)
+
+            val errorResponse = mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
+
+            assertEquals(5, errorResponse.error.size)
+            assertEquals("firstName is missing.", errorResponse.error[0])
+            assertEquals("lastName is missing.", errorResponse.error[1])
+            assertEquals("phoneNumber is missing.", errorResponse.error[2])
+            assertEquals("emailID is missing.", errorResponse.error[3])
+            assertEquals("userName is missing.", errorResponse.error[4])
         }
-        assertEquals(HttpStatus.BAD_REQUEST, exception.status)
-
-        val errorResponse = mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
-
-        assertEquals(5, errorResponse.error.size)
-        assertEquals("firstName is missing.", errorResponse.error[0])
-        assertEquals("lastName is missing.", errorResponse.error[1])
-        assertEquals("phoneNumber is missing.", errorResponse.error[2])
-        assertEquals("emailID is missing.", errorResponse.error[3])
-        assertEquals("userName is missing.", errorResponse.error[4])
     }
 
     @Test
     fun registerNewUserWhileOtherUsersExist() {
-        val user1 = User(firstName = "user1", lastName = "user1", emailId = "user1@example.com", phoneNumber = "+911234567891", username = "user1")
-        val user2 = User(firstName = "user2", lastName = "user2", emailId = "user2@example.com", phoneNumber = "+911234567892", username = "user2")
-        val user3 = User(firstName = "user3", lastName = "user3", emailId = "user3@example.com", phoneNumber = "+911234567893", username = "user3")
+        val user1 = User(
+            firstName = "user1",
+            lastName = "user1",
+            emailId = "user1@example.com",
+            phoneNumber = "+911234567891",
+            username = "user1"
+        )
+        val user2 = User(
+            firstName = "user2",
+            lastName = "user2",
+            emailId = "user2@example.com",
+            phoneNumber = "+911234567892",
+            username = "user2"
+        )
+        val user3 = User(
+            firstName = "user3",
+            lastName = "user3",
+            emailId = "user3@example.com",
+            phoneNumber = "+911234567893",
+            username = "user3"
+        )
         saveUser(user1)
         saveUser(user2)
         saveUser(user3)
-        assertEquals(3,UserRepository.getUserListSize())
+        assertEquals(3, UserRepository.getUserListSize())
 
     }
 }
--- src/test/kotlin/controller/WalletControllerTest.kt
+++ src/test/kotlin/controller/WalletControllerTest.kt
@@ -9,7 +9,6 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import jakarta.inject.Inject
 import models.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
@@ -23,7 +22,6 @@ import services.saveUser
 class
 WalletControllerTest {
 
-    @Inject
     @field:Client("/")
     lateinit var client: HttpClient
 
@@ -39,6 +37,7 @@ WalletControllerTest {
     )
 
     private val walletURI = "/user/user1/addToWallet"
+
     @AfterEach
     fun tearDown() {
         UserRepository.clearUserList()
@@ -61,62 +60,70 @@ WalletControllerTest {
     @Test
     fun shouldNotBeAbleToAddNegativeAmount() {
         val request: HttpRequest<Any> = HttpRequest.POST(walletURI, AddToWalletInput(-10))
-        val exception = assertThrows(HttpClientResponseException::class.java) {
-            client.toBlocking().retrieve(request)
+        if (this::client.isInitialized) {
+            val exception = assertThrows(HttpClientResponseException::class.java) {
+                client.toBlocking().retrieve(request)
+            }
+            assertEquals(HttpStatus.BAD_REQUEST, exception.status)
+            val errorResponse: ErrorResponse =
+                mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
+            assertEquals("Amount added to wallet has to be positive.", errorResponse.error[0])
         }
-        assertEquals(HttpStatus.BAD_REQUEST, exception.status)
-        val errorResponse: ErrorResponse =
-            mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
-        assertEquals("Amount added to wallet has to be positive.", errorResponse.error[0])
     }
 
     @Test
     fun shouldBeAbleToAddAmount() {
         val amountToBeAdded = 10
+        if (this::client.isInitialized) {
 
-        val request: HttpRequest<Any> = HttpRequest.POST(walletURI, AddToWalletInput(amountToBeAdded))
-        val res = client.toBlocking().retrieve(request)
-        val response: WalletResponse = mapper.readValue(res, WalletResponse::class.java)
 
-        //assertEquals()
-        assertEquals("$amountToBeAdded amount added to account", response.message)
-        assertEquals(110, UserRepository.getUser("user1")!!.getFreeMoney()) //100 added in setup
+            val request: HttpRequest<Any> = HttpRequest.POST(walletURI, AddToWalletInput(amountToBeAdded))
+            val res = client.toBlocking().retrieve(request)
+            val response: WalletResponse = mapper.readValue(res, WalletResponse::class.java)
+
+            //assertEquals()
+            assertEquals("$amountToBeAdded amount added to account", response.message)
+            assertEquals(110, UserRepository.getUser("user1")!!.getFreeMoney()) //100 added in setup
+        }
     }
 
     @Test
     fun shouldNotAbleToAddAmountForInvalidUser() {
         val amountToBeAdded = 10
         val request: HttpRequest<Any> = HttpRequest.POST("/user/user2/addToWallet", AddToWalletInput(amountToBeAdded))
+        if (this::client.isInitialized) {
+            val exception = assertThrows(HttpClientResponseException::class.java) {
+                client.toBlocking().retrieve(request)
+            }
 
-        val exception = assertThrows(HttpClientResponseException::class.java) {
-            client.toBlocking().retrieve(request)
+            val response: ErrorResponse =
+                mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
+
+            assertEquals(HttpStatus.NOT_FOUND, exception.status)
+            assertEquals("User does not exist", response.error[0])
         }
-
-        val response: ErrorResponse =
-            mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
-
-        assertEquals(HttpStatus.NOT_FOUND, exception.status)
-        assertEquals("User does not exist", response.error[0])
-
     }
 
     @Test
     fun shouldNotAbleToAddMoreAmountThanSetLimit() {
         val amountToBeAdded = DataStorage.MAX_AMOUNT + 1
 
-        val request: HttpRequest<Any> = HttpRequest.POST(walletURI, AddToWalletInput(amountToBeAdded))
+        if (this::client.isInitialized) {
+            val request: HttpRequest<Any> = HttpRequest.POST(walletURI, AddToWalletInput(amountToBeAdded))
 
-        val exception = assertThrows(HttpClientResponseException::class.java) {
-            client.toBlocking().retrieve(request)
+            val exception = assertThrows(HttpClientResponseException::class.java) {
+                client.toBlocking().retrieve(request)
+            }
+
+
+            val response: ErrorResponse =
+                mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
+
+            assertEquals(HttpStatus.BAD_REQUEST, exception.status)
+            assertEquals(
+                "Amount exceeds maximum wallet limit. Wallet range 0 to ${DataStorage.MAX_AMOUNT}",
+                response.error[0]
+            )
         }
-
-        val response: ErrorResponse =
-            mapper.readValue(exception.response.body()!!.toString(), ErrorResponse::class.java)
-
-        assertEquals(HttpStatus.BAD_REQUEST, exception.status)
-        assertEquals(
-            "Amount exceeds maximum wallet limit. Wallet range 0 to ${DataStorage.MAX_AMOUNT}",
-            response.error[0]
-        )
     }
 }
